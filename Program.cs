2cf4684d01222ce1ff262bd6c5c98c4e649cbbad
jeangalvis <campusjean2023@gmail.com>
2023-12-04 20:34:55 -0500

    reto5parte1

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
-﻿Console.Write("Ingresa el SamDaily: ");
+﻿
+Console.Write("Ingresa el SamDaily: ");
 int samDaily = Convert.ToInt32(Console.ReadLine());
 Console.Write("Ingresa el KellyDaily: ");
 int kellyDaily = Convert.ToInt32(Console.ReadLine());
 Console.Write("Ingresa el Difference: ");
 int difference = Convert.ToInt32(Console.ReadLine());
+
+
 int validador = 0;
 int contador = 0;
 int samsolved = 0;
 int kellysolved = 0;
 while (validador !=1){
+    if(samDaily >= kellyDaily){
+        contador = -1;
+        validador = 1;
+    }
     contador ++;
     if(contador == 1){
         samsolved = samDaily + difference;
